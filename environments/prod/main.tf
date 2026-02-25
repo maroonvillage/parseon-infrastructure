ba5f2be2a5b140f2ed4137c2f384a0204016252e
Cole P <maroonyear0@gmail.com>
2026-02-24 16:47:34 -0800

    Add Terraform configuration for dev and prod environments with modules

---
+++ environments/prod/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "s3" {
+    bucket = "parseon-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+}
+
+module "security_groups" {
+  source = "../../modules/security_groups"
+}
+
+module "rds" {
+  source = "../../modules/rds"
+}
+
+module "ecs_cluster" {
+  source = "../../modules/ecs_cluster"
+}
+
+module "ecs_service" {
+  source = "../../modules/ecs_service"
+}
+
+module "s3" {
+  source = "../../modules/s3"
+}
+
+module "sqs" {
+  source = "../../modules/sqs"
+}
+
+module "alb" {
+  source = "../../modules/alb"
+}
+
+module "iam" {
+  source = "../../modules/iam"
+}
+
+module "cloudfront" {
+  source = "../../modules/cloudfront"
+}
