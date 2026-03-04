5e9e1a420432a1e43699d839a99db5159c8aa80e
Cole P <maroonyear0@gmail.com>
2026-03-04 09:00:51 -0800

    Fix CloudWatch log driver configuration to use region ID instead of name

--- modules/ecs_service/main.tf
+++ modules/ecs_service/main.tf
@@ -26,7 +26,7 @@ resource "aws_ecs_task_definition" "this" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.ecs.name
-          awslogs-region        = data.aws_region.current.name
+          awslogs-region        = data.aws_region.current.id
           awslogs-stream-prefix = "ecs"
         }
       }
